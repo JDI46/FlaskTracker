7c3f1c467d92a167609739076aeefd9f9e7e00c6
Xander Whitby <xwhitby20@gmail.com>
2024-06-16 18:18:10 -0600

    Testing templates out

--- MainF.py
+++ MainF.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<p>Welcome to my Website!</p>'
+    return render_template('base.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
