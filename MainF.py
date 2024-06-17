3feea0d1506134c4d9b98fe1174ba71702e8c0ec
Xander Whitby <xwhitby20@gmail.com>
2024-06-16 19:04:13 -0600

    Added different html files for home page redirect and message me

--- MainF.py
+++ MainF.py
@@ -1,20 +1,22 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('base.html')
+    return render_template("index.html")
 
-
-@app.route('/login', methods=['Post', 'GET'])
+@app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
-	    user = request.form["nm"]
+        user = request.form["nm"]
         return redirect(url_for("user", usr=user))
     else:
-	    return render_template("login.html")
+        return render_template("login.html")
 
-if __name__ == '__main__':
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
+
+if __name__ == "__main__":
     app.run(debug=True)
-
