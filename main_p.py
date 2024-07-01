b0401360126ce16214c7193a48d50069275e60d1
Xander Whitby <xwhitby20@gmail.com>
2024-07-01 16:28:05 -0600

    Got redirect after inputing password to work

---
+++ main_p.py
@@ -0,0 +1,35 @@
+from flask import Flask, redirect, url_for, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/home")
+def base():
+    return render_template("home.html")
+
+@app.route('/admin')
+def home():
+    return render_template('admin_page.html')
+
+
+
+
+# @app.route('/messageme')
+# me():
+#     return render_template('messageme.html')
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("login.html")
+
+@app.route("/admin_page")
+def user():
+    return render_template("admin_page.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
